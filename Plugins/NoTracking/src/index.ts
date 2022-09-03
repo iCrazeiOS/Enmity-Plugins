f9740e8199a7921459babcb3aeeff62b67bbeaef
iCrazeiOS <icrazeios@protonmail.com>
2022-09-03 23:03:00 +0100

    fix? (v1.1.1)

--- Plugins/NoTracking/src/index.ts
+++ Plugins/NoTracking/src/index.ts
@@ -29,35 +29,32 @@ const NoTracking: Plugin = {
 			const urlRegex = /(http|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g;
 
 			message.match(urlRegex)?.forEach(url => {
+				let newUrl;
 				if (url.includes("twitter.com/")) {
-					let newUrl = url.split("t=")[0].split("s=")[0];
-					if (newUrl.endsWith("?")) newUrl = newUrl.slice(0, -1);
-					message = message.replace(url, newUrl);
+					newUrl = url.split("t=")[0].split("s=")[0];
 				} else if (url.includes("reddit.com/")) {
-					// do some stuff
 					// (?context is kept because that actually changes how replies are displayed)
-					let newUrl = url.replace(/utm_medium(=[^&]*)?|^utm_medium(=[^&]*)?&?/g, "");
+					newUrl = url.replace(/utm_medium(=[^&]*)?|^utm_medium(=[^&]*)?&?/g, "");
 					newUrl = newUrl.replace(/utm_source(=[^&]*)?|^utm_source(=[^&]*)?&?/g, "");
 					newUrl = newUrl.replace(/utm_name(=[^&]*)?|^utm_name(=[^&]*)?&?/g, "");
 					newUrl = newUrl.replace("&&", "");
 					newUrl = newUrl.replace("?&", "?");
 					if (newUrl.endsWith("&")) newUrl = newUrl.slice(0, -1);
-					message = message.replace(url, newUrl);
 				} else if (url.includes("ebay.co")) { // com / co.uk / whatever
-					let newUrl = url.split("?")[0]; // remove all params
+					newUrl = url.split("?")[0]; // remove all params
 					let params = getParams(url);
 					if (params["_nkw"]) newUrl += "?_nkw=" + params["_nkw"]; // add the search term back, if there was one
-					message = message.replace(url, newUrl);
 				} else if (url.includes("soundcloud.com/")) {
-					let newUrl = url.split("?")[0]; // remove all params, don't think any are needed
-					message = message.replace(url, newUrl);
+					newUrl = url.split("?")[0]; // remove all params, don't think any are needed
 				} else if (url.includes("instagram.com/")) {
-					let newUrl = url.split("?")[0]; // remove all params, don't think any are needed
+					newUrl = url.split("?")[0]; // remove all params, don't think any are needed
+				}
+
+				if (newUrl) {
+					if (newUrl.endsWith("?")) newUrl = newUrl.slice(0, -1);
 					message = message.replace(url, newUrl);
 				}
 			});
-
-			if (message.endsWith("?")) message = message.slice(0, -1);
 
 			a1[1]["content"] = message;
 		});
