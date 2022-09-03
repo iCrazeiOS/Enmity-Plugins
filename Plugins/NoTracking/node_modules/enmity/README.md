a49f42fc1ee93b975396cc97ab11c95bd7748a67
iCrazeiOS <icrazeios@protonmail.com>
2022-09-03 14:36:01 +0100

    NoTracking v1.1

---
+++ Plugins/NoTracking/node_modules/enmity/README.md
@@ -0,0 +1,19 @@
+<div align='center'>
+   <img src='https://repo.enmity.app/banner.png' />
+</div>
+
+---
+
+<div align='center'>
+   <h3>Enmity API</h3>
+
+   API wrapper for Enmity.<br />
+   Documentation is available on [docs.enmity.app](https://docs.enmity.app).
+</div>
+
+---
+
+<div align='center' style='margin-top: 15px;'>
+   <a href='https://discord.gg/rMdzhWUaGT'><img align='center' alt='Discord' src='https://img.shields.io/discord/950850315601711176?color=36309d&label=DISCORD&logo=discord&logoColor=white&style=for-the-badge'></a>
+   <a href='https://twitter.com/EnmityApp'><img align='center' alt='Twitter' src='https://img.shields.io/twitter/follow/EnmityApp?color=36309d&label=TWITTER&logo=TWITTER&logoColor=white&style=for-the-badge'></a>
+</div>
--- Plugins/NoTracking/src/index.ts
+++ Plugins/NoTracking/src/index.ts
@@ -5,6 +5,19 @@ import manifest from "../manifest.json";
 
 const Patcher = create("no-tracking");
 
+// stolen from https://stackoverflow.com/a/10126995/14028635
+function getParams(str) {
+	let  params = {};
+	let keyValPairs = str.replace(/.*?\?/,"").split('&');
+	for (let pairNum in keyValPairs) {
+		let key = keyValPairs[pairNum].split('=')[0];
+		if (!key.length) continue;
+		if (typeof params[key] === 'undefined') params[key] = [];
+		params[key].push(keyValPairs[pairNum].split('=')[1]);
+	}
+	return params;
+}
+
 const NoTracking: Plugin = {
 	...manifest,
 	
@@ -23,10 +36,23 @@ const NoTracking: Plugin = {
 				} else if (url.includes("reddit.com/")) {
 					// do some stuff
 					// (?context is kept because that actually changes how replies are displayed)
-					let newUrl = url.replace(/utm_medium(=[^&]*)?|^utm_medium(=[^&]*)?&?/g, "").replace(/utm_source(=[^&]*)?|^utm_source(=[^&]*)?&?/g, "");
+					let newUrl = url.replace(/utm_medium(=[^&]*)?|^utm_medium(=[^&]*)?&?/g, "");
+					newUrl = newUrl.replace(/utm_source(=[^&]*)?|^utm_source(=[^&]*)?&?/g, "");
+					newUrl = newUrl.replace(/utm_name(=[^&]*)?|^utm_name(=[^&]*)?&?/g, "");
 					newUrl = newUrl.replace("&&", "");
 					newUrl = newUrl.replace("?&", "?");
 					if (newUrl.endsWith("&")) newUrl = newUrl.slice(0, -1);
+					message = message.replace(url, newUrl);
+				} else if (url.includes("ebay.co")) { // com / co.uk / whatever
+					let newUrl = url.split("?")[0]; // remove all params
+					let params = getParams(url);
+					if (params["_nkw"]) newUrl += "?_nkw=" + params["_nkw"]; // add the search term back, if there was one
+					message = message.replace(url, newUrl);
+				} else if (url.includes("soundcloud.com/")) {
+					let newUrl = url.split("?")[0]; // remove all params, don't think any are needed
+					message = message.replace(url, newUrl);
+				} else if (url.includes("instagram.com/")) {
+					let newUrl = url.split("?")[0]; // remove all params, don't think any are needed
 					message = message.replace(url, newUrl);
 				}
 			});
